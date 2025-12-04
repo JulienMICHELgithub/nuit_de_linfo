435d7a401036c1409aff05168022dcf980a71192
muscularbird <etienne.kretz@epitech.eu>
2025-12-04 20:36:16 +0100

    [~] change scene

---
+++ my-phaser-app/src/game/scenes/MainMenu.ts
@@ -0,0 +1,102 @@
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+import { Scene } from 'phaser';
+/* START-USER-IMPORTS */
+import { EventBus } from '../EventBus';
+/* END-USER-IMPORTS */
+
+export default class MainMenu extends Scene {
+
+	constructor() {
+		super("MainMenu");
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	editorCreate(): void {
+
+		// background
+		this.add.image(517, 170, "background");
+
+		// logo
+		const logo = this.add.image(515, 167, "logo");
+
+		// start_button
+		const start_button = this.add.image(530, 441, "start-button");
+		start_button.setInteractive(this.input.makePixelPerfect());
+		start_button.scaleX = 0.5;
+		start_button.scaleY = 0.5;
+
+		this.logo = logo;
+
+		this.events.emit("scene-awake");
+	}
+
+	private logo!: Phaser.GameObjects.Image;
+
+	/* START-USER-CODE */
+    logoTween: Phaser.Tweens.Tween | null;
+
+	// Write your code here
+    create ()
+    {
+        this.editorCreate();
+
+        EventBus.emit('current-scene-ready', this);
+    }
+
+    changeScene ()
+    {
+        if (this.logoTween)
+        {
+            this.logoTween.stop();
+            this.logoTween = null;
+        }
+
+        this.scene.start('Game');
+    }
+
+    moveLogo (vueCallback: ({ x, y }: { x: number, y: number }) => void)
+    {
+        if (this.logoTween)
+        {
+            if (this.logoTween.isPlaying())
+            {
+                this.logoTween.pause();
+            }
+            else
+            {
+                this.logoTween.play();
+            }
+        } 
+        else
+        {
+            this.logoTween = this.tweens.add({
+                targets: this.logo,
+                x: { value: 750, duration: 3000, ease: 'Back.easeInOut' },
+                y: { value: 80, duration: 1500, ease: 'Sine.easeOut' },
+                yoyo: true,
+                repeat: -1,
+                onUpdate: () => {
+                    if (vueCallback)
+                    {
+                        vueCallback({
+                            x: Math.floor(this.logo.x),
+                            y: Math.floor(this.logo.y)
+                        });
+                    }
+                }
+            });
+        }
+    }
+    /* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here
+export { MainMenu };
