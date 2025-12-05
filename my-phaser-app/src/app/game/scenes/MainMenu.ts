b7ffa568699219c1d598e0874bf21d7e8d4b8817
major <julien.michel1026@icloud.com>
2025-12-05 02:37:57 +0100

    [+] umamusume

---
+++ my-phaser-app/src/app/game/scenes/MainMenu.ts
@@ -0,0 +1,166 @@
+@@ -1,55 +0,0 @@
+
+
+import * as Phaser from 'phaser';
+
+
+import { EventBus } from '../EventBus';
+
+
+
+
+
+export default class MainMenu extends Phaser.Scene {
+
+
+    private logo!: Phaser.GameObjects.Image;
+
+
+    private startButton!: Phaser.GameObjects.Image;
+
+
+    private logoTween!: Phaser.Tweens.Tween | null;
+
+
+
+
+
+    constructor() {
+
+
+        super("MainMenu");
+
+
+        this.logoTween = null;
+
+
+    }
+
+
+
+
+
+    preload() {
+
+
+        // Charger les assets
+
+
+        this.load.image('background', 'https://labs.phaser.io/assets/skies/space3.png');
+
+
+        this.load.image('logo', 'https://labs.phaser.io/assets/sprites/phaser3-logo.png');
+
+
+        this.load.image('start-button', '/assets/start-button.png');
+
+
+    }
+
+
+
+
+
+    create() {
+
+
+        // Background
+
+
+        this.add.image(400, 300, 'background');
+
+
+
+
+
+        // Logo
+
+
+        this.logo = this.add.image(400, 150, 'logo');
+
+
+        this.logo.setScale(0.5);
+
+
+
+
+
+        // Tween du logo
+
+
+        this.logoTween = this.tweens.add({
+
+
+            targets: this.logo,
+
+
+            y: 170,
+
+
+            duration: 1500,
+
+
+            ease: 'Sine.easeInOut',
+
+
+            yoyo: true,
+
+
+            repeat: -1
+
+
+        });
+
+
+
+
+
+        // Bouton "Play"
+
+
+        this.startButton = this.add.image(400, 400, 'start-button');
+
+
+        this.startButton.setScale(0.5);
+
+
+        this.startButton.setInteractive({ useHandCursor: true });
+
+
+
+
+
+        // Hover effect
+
+
+        this.startButton.on('pointerover', () => this.startButton.setTint(0xaaaaaa));
+
+
+        this.startButton.on('pointerout', () => this.startButton.clearTint());
+
+
+
+
+
+        // Click -> lancer GameScene
+
+
+        this.startButton.on('pointerdown', () => {
+
+
+            this.scene.start('Game');
+
+
+        });
+
+
+
+
+
+        EventBus.emit('current-scene-ready', this);
+
+
+    }
+
+
+}
