8d444bf8322de16f91b919cc1de0ae15b10ffe47
Nael <nael.de-luca@epitech.eu>
2025-12-05 02:02:03 +0100

    dialog début

---
+++ my-phaser-app/src/components/Message.tsx
@@ -0,0 +1,52 @@
+import React, { useMemo } from 'react';
+import { animated, useTransition } from 'react-spring';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    dialogMessage: () => ({
+        fontSize: '12px',
+        textTransform: 'uppercase',
+    }),
+}));
+
+const Message = ({
+    message = [],
+    trail = 35,
+    onMessageEnded = () => {},
+    forceShowFullMessage = false,
+}) => {
+    const classes = useStyles();
+    const items = useMemo(
+        () => message.trim().split('').map((letter, index) => ({
+            item: letter,
+            key: index,
+        })),
+        [message]
+    );
+
+    const transitions = useTransition(items, {
+        trail,
+        from: { display: 'none' },
+        enter: { display: '' },
+        onRest: (status, controller, item) => {
+            if (item.key === items.length - 1) {
+                onMessageEnded();
+            }
+        },
+    });
+
+    return (
+        <div className={classes.dialogMessage}>
+            {forceShowFullMessage && (
+                <span>{message}</span>
+            )}
+            {!forceShowFullMessage && transitions((styles, { item, key }) => (
+                <animated.span key={key} style={styles}>
+                    {item}
+                </animated.span>
+            ))}
+        </div>
+    );
+};
+
+export default Message;
